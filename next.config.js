c25e51fccc172a55a79dd7a413fd135ed29ef303
Dan Tran <picasdan99@gmail.com>
2021-11-04 12:13:03 +0800

    Update next config base path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: './',
+  basePath: isProd ? '/cs5340-notes' : '',
+  assetPrefix: isProd ? '/cs5340-notes' : '',
 };
